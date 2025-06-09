06a743203107922b5b7bbfd39e38a3b82adcbe7f
9400 <9400@veritas.edu.ng>
2025-06-09 04:50:21 +0100

    question2 done check readme file for login credentials

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SEN-202-EXAM_9400
 sen 202 exam
 9400 this is my exam it is an employee manager system with django
+admin credentials username:emma   passward:12345
 
 |**Name**       | **Github username that links to account**         | **Matric Number** |	**Contribution** |
 |---------------|---------------------------------------------------|-------------------|------------------|
--- aromaekrika_emmanuel_staff_apii/employees/models.py
+++ aromaekrika_emmanuel_staff_apii/employees/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class StaffBase(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Manager(models.Manager):
+        manager_name =models.CharField(max_length=100)
+        department =models.CharField(max_length=100)
+        has_company_card = models.BooleanField(default=True)
+
+
+
+    class Intern(models.Manager):
+        Intern_name =models.CharField(max_length=100)
+        mentor =models.CharField(max_length=100)
+        internship_end = models.DateField
+
+
+
+
+
